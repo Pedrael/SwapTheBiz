7c69b775c701ac638ab97dfc42570328b7d28a43
Pedrael <45235838+Pedrael@users.noreply.github.com>
2020-06-01 14:28:45 +0300

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+import jQuery from 'jquery';
+import Header from './header.js';
+import Carousel from './carousel.js';
+//import MyTimeIsNow from './date.js';
+//import Mail from './mail.js';
+import Ripple from './ripple.js';
+import Parallax from './parallax.js';
+//require("./scrollvideo.js");
+
+Header();
+Carousel();
+//Mail();
+//MyTimeIsNow("date-input");
+Ripple();
+Parallax();
